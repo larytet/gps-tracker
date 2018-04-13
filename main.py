9285ef317d6da5ed8e1e2fc265ec626c675ec2aa
arkady <larytet@yahoo.com>
2018-04-13 19:35:21 +0300

    Script to catch the GPS data

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ I am getting something like
 3G*1452592884*005F*UD2,130418,145006,V,32.180737,N,34.8552780
 '''
 def get_coordinates(data):
-    m = re.match("\[3G.+,([0-9.]+),N,([0-9.]+),.+\]", data)
+    m = re.match("\[3G.([0-9]+).+,([0-9.]+),N,([0-9.]+),.+\]", data)
 
     result = (m != None)
     id, c1, c2 = None, None, None
