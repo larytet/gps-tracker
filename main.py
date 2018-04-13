017c15b52791e6f998363cc25d5c7f2005c48c49
arkady <larytet@yahoo.com>
2018-04-13 18:22:12 +0300

    Script to catch the GPS data

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Create an INET, STREAMing socket
 '''
 def open_server_socket(port=4444):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    hostname = socket.gethostname()
+    hostname = "127.0.0.1"#socket.gethostname()
     s.bind((hostname, port))
     print("Bound {0}:{1}".format(hostname, port))
     return s
