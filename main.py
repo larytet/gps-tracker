55618c4b6dddd429e576c31f9f01c903f29811ca
arkady <larytet@yahoo.com>
2018-04-13 20:02:19 +0300

    Script to catch the GPS data

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ try:
     accept_loop()
 except KeyboardInterrupt:
     print("I got Ctrl-C, exiting")
-    for (clientsocket, address, thread_aborted), ct in clients.iteritems():
-        thread_aborted[0] = True
+    for (clientsocket, address), ct in clients.iteritems():
+        address.thread_aborted = True
         print("Close {0}".format(address))
         close_socket(clientsocket, address)
 
