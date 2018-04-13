d3f8abd00f8bb4750bbfdec3063fd1d7bab1c234
arkady <larytet@yahoo.com>
2018-04-13 20:01:39 +0300

    Script to catch the GPS data

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def accept_loop():
         
         stopwatch = Stopwatch()
         print("Accepted connection from {0}".format(address))
-        thread_aborted = (False)
+        address.thread_aborted = False
         ct = threading.Thread(target=client_thread, args=(clientsocket,address, stopwatch, thread_aborted))
         
         ct.run()
