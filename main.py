65faea451b2d9b00224deaa3c0a846ca18e95361
arkady <larytet@yahoo.com>
2018-04-13 21:09:01 +0300

    Script to catch the GPS data

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def client_thread(clientsocket, address, stopwatch):
             if stopwatch.thread_aborted:
                 print("{0}: Aborting thread {1}".format(stopwatch.elapsed_str(), address))
                 break
-            # probably timeut 
+            # probably timeout 
             continue
         clientsocket.send("[OK]\n")
         result, id, c1, c2 = get_coordinates(data)
