0d6e4203ff331d2c9d820a740603ea2407440d94
arkady <larytet@yahoo.com>
2018-04-14 20:23:39 +0300

    Script to catch the GPS data

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def client_thread(clientsocket, address, stopwatch):
             print("{0}: {1} {2} {3} from {4}".format(stopwatch.elapsed_str(), str(timestamp), id, url, address))
             break
         elif result == ParsingResult.Prompt:
-            print("{0}: Heart beat {2} {3} from {4}".format(stopwatch.elapsed_str(), str(timestamp), id, url, address))
+            print("{0}: Heart beat {1} {2} from {3}".format(stopwatch.elapsed_str(), str(timestamp), id, address))
         elif result == ParsingResult.Failed:
             print("{0}: Failed to parse '{1}' from {2}".format(stopwatch.elapsed_str(), data, address))
 
